f29aabc2c9634f8cd90f3ae02560e796126644ee
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-17 20:01:04 +0500

    3 и 5 Выведены в перменные Приделы рандома тыгже выведенны в переменные

--- homeWork_Random/Program.cs
+++ homeWork_Random/Program.cs
@@ -10,24 +10,29 @@ namespace homeWork_Random
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int number = rand.Next(0, 101);
+            Random random = new Random();
+            int lowerLimitRandom = 0;
+            int upperLimitRandom = 101;
+            int number = random.Next(lowerLimitRandom, upperLimitRandom);
             int sumNumbers = number;
+            int firstDivider = 3;
+            int secondDivider = 5;
 
             Console.WriteLine("Рандомное число: " + number);
 
             for (int i = 0; i < number; i++) 
             {
-                if ((i % 5) == 0)
+                if ((i % firstDivider) == 0)
                 {
                     sumNumbers += i;
                 }
-                else if ((i % 3) == 0) 
+                else if ((i % secondDivider) == 0) 
                 {
                     sumNumbers += i;
                 }
             }
-            Console.WriteLine("Сумма всех пложительных чисел кратных 3 или 5 до " + number +"(включаяя само число), равно " + sumNumbers);
+
+            Console.WriteLine($"Сумма всех пложительных чисел кратных 3 или 5 до {number}(включаяя само число), равно {sumNumbers}");
             Console.ReadKey();
         }
     }
